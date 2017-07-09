a8d5922aab2de8c7757312a595362f48a196e12b
Staremang <Staremang@ya.ru>
2017-07-10 01:35:45 +0700

    Preview for mobile

--- js-source/main.js
+++ js-source/main.js
@@ -2,7 +2,7 @@ var player,
 	done = false;
 
 function isMobile () {
-	if ($(window).width() <= 768) {
+	if ($(window).width() <= 575) {
 		return true;
 	} else {
 		return false;
@@ -89,6 +89,9 @@ function onPlayerReady(event) {
 	})
 }
 
+var beforePage = function () {},
+	afterPage = function () {};
+
 function pausePlayVideo() {
 	
 	if ($('body').hasClass('disabled-onepage-scroll')) {
@@ -105,27 +108,22 @@ function pausePlayVideo() {
 		}
 	}
 	
-	
-	
-	$(".wrapper").onepage_scroll({
-		updateURL: true,
-		loop: false,
-		beforeMove: function(index) {
-			if (index == 1 && done) {
-				player.playVideo();
-	//			console.log('play');
-				done = false;
-			}
-		},
-		afterMove: function(index) {
-			if (index !== 1 && !done) {
-				player.pauseVideo();
-	//			console.log('pause')
-				done = true;
-			}
+	beforePage = function(index) {
+		if (index == 1 && done) {
+			player.playVideo();
+				console.log('play');
+			done = false;
 		}
-	});
+	}
+	afterPage = function (index) {
+		if (index !== 1 && !done) {
+			player.pauseVideo();
+				console.log('pause')
+			done = true;
+		}
+	}
 }
+
 $(document).ready(function () {
 	var tag = document.createElement('script');
 
@@ -141,20 +139,36 @@ $(document).ready(function () {
 	document.querySelector('.header-display__btn').addEventListener('click', function () {
 		document.querySelector('.header-container').classList.toggle('view')	
 	})
-	
-	$(".wrapper").onepage_scroll({
-		sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-	//	easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-	//									// "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-	//	animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-	//	pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-		updateURL: true,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-		loop: false,                    // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-	//	keyboard: true,                  // You can activate the keyboard controls
-//		responsiveFallback: true,       // You can fallback to normal page scroll by defining the width of the browser in which
-	//									// you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-	//									// the browser's width is less than 600, the fallback will kick in.
-	});
+	if (!isMobile()) {
+		$('body').addClass('onepage');
+		$(".wrapper").onepage_scroll({
+			sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+		//	easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+		//									// "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+		//	animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+	//		pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+			updateURL: true,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+			loop: false,                    // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+			beforeMove: function (index) {
+				beforePage(index);
+			},
+			afterMove: function (index) {
+				afterPage(index);
+			},
+		//	keyboard: true,                  // You can activate the keyboard controls
+			responsiveFallback: 546,       // You can fallback to normal page scroll by defining the width of the browser in which
+											// you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+											// the browser's width is less than 600, the fallback will kick in.
+		});
+	}
+//	setTimeout(function () {
+//		test = function () {
+//			console.log('second');
+//		}
+//	}, 5000)
+//	setInterval(function() {
+//		test();
+//	}, 1000)
 	$('.section-hero-mouse-icon').click(function () {
 		$(".wrapper").moveDown();
 	})
@@ -174,6 +188,45 @@ $(document).ready(function () {
 //		autoplay:true,
 //		autoplayTimeout:5000,
 //		autoplayHoverPause:true
+	})
+	
+	function setSlider (name, param) {
+		var trigger = isMobile();
+		if (isMobile()) {
+			$(name).addClass('owl-carousel');
+			$(name).addClass('owl-theme');
+			$(name).owlCarousel(param)
+		} else {
+			$(name).removeClass('owl-carousel');
+			$(name).removeClass('owl-theme');
+			$(name).trigger('destroy.owl.carousel');
+		}
+		
+		$(window).resize(function () {
+			if (isMobile() && !trigger) {
+				trigger = true;
+				
+				$(name).addClass('owl-carousel');
+				$(name).addClass('owl-theme');
+				$(name).owlCarousel(param)
+			} else if (!isMobile() && trigger) {
+				trigger = false;
+				
+				$(name).removeClass('owl-carousel');
+				$(name).removeClass('owl-theme');
+				$(name).trigger('destroy.owl.carousel');
+			}
+		})
+	}
+	setSlider('.about-slider', {
+		loop:true,
+		nav:false,
+		items:1
+	});
+	setSlider('.vacancy-container', {
+		loop:true,
+		nav:false,
+		items:1
 	})
 //	$('.vacancy-container').owlCarousel({
 //		loop:true,
